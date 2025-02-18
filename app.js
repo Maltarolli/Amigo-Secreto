a1b766ca9c01be20402887ecf97df384a356b9e0
Pedro Roberto <maltarollipedro@gmail.com>
2025-02-18 14:41:06 -0300

    Adicionando sorteio e mensagem

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ function adicionarAmigo(){
     let input_amigo = document.querySelector('#amigo')
     let entrada_amigo = input_amigo.value
     if (entrada_amigo.length == 0) {
-        alert('Por facor insira um nome')
+        alert('Por favor insira um nome')
     }else{
         amigos.push(entrada_amigo)
-        console.log(amigos)
         listaAmigo(amigos)
     }
     input_amigo.value = ''
@@ -19,5 +18,17 @@ function listaAmigo(amigos){
     for (let i = 0; i < amigos.length; i++) {
         listaAmigos.innerHTML += `<li>${amigos[i]}</li>`
         
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length >= 2) {
+        let numeroAleatorio = Math.floor(Math.random() * (amigos.length - 0) + 0)
+        let sorteado = amigos[numeroAleatorio]
+        let listaAmigos = document.querySelector('#listaAmigos')
+        listaAmigos.innerHTML = `O amigo secreto sorteado é: ${sorteado}.`
+        listaAmigos.classList.add('sorteado')
+    }else{
+        alert("Por favor insira mais de 1 nome para continuar")
     }
 }
