aacb992cf7463cc218e1d2f41ca9fbef459884f8
Pedro Roberto <maltarollipedro@gmail.com>
2025-02-18 09:39:02 -0300

    Input de entrada de nome e lista de nomes

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo(){
+    let input_amigo = document.querySelector('#amigo')
+    let entrada_amigo = input_amigo.value
+    if (entrada_amigo.length == 0) {
+        alert('Por facor insira um nome')
+    }else{
+        amigos.push(entrada_amigo)
+        console.log(amigos)
+        listaAmigo(amigos)
+    }
+    input_amigo.value = ''
+}
+
+function listaAmigo(amigos){
+    let listaAmigos = document.querySelector('#listaAmigos')
+    listaAmigos.innerHTML = ''
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`
+        
+    }
+}
